fb7121841ad18212e2550127771127212310d154
themarioga <marioga2512@gmail.com>
2018-12-01 19:24:44 +0100

    Top menu created

---
+++ Assets/Scripts/HUD/TopPanelController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TopPanelController : MonoBehaviour {
+
+	[SerializeField]
+	private TextMeshProUGUI goldText;
+	[SerializeField]
+	private Slider foodSlider;
+	[SerializeField]
+	private Slider rumSlider;
+	[SerializeField]
+	private Slider woodSlider;
+	[SerializeField]
+	private Slider cannonSlider;
+	[SerializeField]
+	private Slider peopleSlider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+	
+}
