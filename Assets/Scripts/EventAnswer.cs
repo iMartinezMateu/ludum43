d1390e36524629ad6262d62779f4cf612a090450
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-12-02 17:00:02 +0100

    Add Event and EventAnswer

---
+++ Assets/Scripts/EventAnswer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public enum Type
+{
+    BOOTY,PIECES,CREW,RUM,FOOD,GUNS
+}
+
+public class EventAnswer : MonoBehaviour
+{
+    public String answer;
+    public Type powerupType;
+    public int powerupTypeQuantity;
+    public Type decreaseType;
+    public int decreaseTypeQuantity;
+}
