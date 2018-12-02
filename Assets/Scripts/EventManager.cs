8f004f43bf4ac6cf89e04c6ada2dd4bb9802b6f0
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-12-02 18:56:26 +0100

    Add Event Manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EventManager : MonoBehaviour {
+	[SerializeField]
+	private EventPanelController eventPanelController;
+	[SerializeField]
+	private int eventTextTimeOnScreen;
+
+	private Event curGoodEvent;
+	private Event curBadEvent;
+	
+	private EventLists events;
+
+	void Start()
+	{
+		var jsonString = Resources.Load<TextAsset>("test");
+		events = JsonMapper.ToObject<EventLists>(jsonString.text);
+		foreach (Event item in events.goodEvents)
+		{
+			curGoodEvent = item;
+			Invoke("InvokePositiveEvent", Random.Range(1,10)*item.appearanceProbability);
+
+		}
+		foreach (Event item in events.badEvents)
+		{
+			curBadEvent = item;
+			Invoke("InvokeNegativeEvent", Random.Range(1,10)*item.appearanceProbability);
+		}
+
+	}
+
+	void InvokePositiveEvent() {
+			if (curGoodEvent.answers.Length < 2)
+			{
+				eventPanelController.ShowEventWithoutOptions(curGoodEvent.text, eventTextTimeOnScreen);
+			}
+			else
+			{
+				List<string> options = new List<string>();
+				foreach (EventAnswer answer in curGoodEvent.answers)
+				{
+					options.Add(answer.text);
+				}
+				eventPanelController.ShowEventWithOptions(curGoodEvent.text, options);
+			}
+		
+		Invoke("InvokePositiveEvent", Random.Range(1,10)*curGoodEvent.appearanceProbability);
+
+		}
+	
+	
+	void InvokeNegativeEvent() {
+			if (curBadEvent.answers.Length < 2)
+			{
+				eventPanelController.ShowEventWithoutOptions(curBadEvent.text, eventTextTimeOnScreen);	
+			}
+			else
+			{
+				List<string> options = new List<string>();
+				foreach (EventAnswer answer in curBadEvent.answers)
+				{
+					options.Add(answer.text);
+				}
+
+				eventPanelController.ShowEventWithOptions(curBadEvent.text, options);
+			}
+		Invoke("InvokeNegativeEvent", Random.Range(1,10)*curBadEvent.appearanceProbability);
+
+		}
+	
+}
