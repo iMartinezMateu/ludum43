33d961f24d7cba047694fbc88ba31464b50371f4
Iván Martínez Mateu <ivanmartinezmateu@icloud.com>
2018-12-02 20:52:32 +0100

    Update Event Manager, associate button events

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using LitJson;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class EventManager : MonoBehaviour
 {
 	public HUDManager hudManager;
+	public ResourceManager resourceManager;
 
 	private Event curGoodEvent;
 	private Event curBadEvent;
@@ -34,7 +36,7 @@ public class EventManager : MonoBehaviour
 		foreach (EventAnswer answer in curGoodEvent.answers) {
 			options.Add (answer.text);
 		}
-
+		hudManager.AddClickEventOption(GetGoodDialogButton);
 		hudManager.ShowEvent (curGoodEvent.text, options);
 
 		Invoke ("InvokePositiveEvent", Random.Range (1, 10) * curGoodEvent.appearanceProbability);
@@ -45,10 +47,66 @@ public class EventManager : MonoBehaviour
 		foreach (EventAnswer answer in curBadEvent.answers) {
 			options.Add (answer.text);
 		}
-
+		hudManager.AddClickEventOption(GetBadDialogButton);
 		hudManager.ShowEvent (curBadEvent.text, options);
+
 
 		Invoke ("InvokeNegativeEvent", Random.Range (1, 10) * curBadEvent.appearanceProbability);
 	}
+	
+	void GetGoodDialogButton(int n) {
+		foreach (Balance balance in curGoodEvent.answers[n].balances)
+		{
+			switch (balance.type)
+			{
+				case ResourceType.RUM:
+					resourceManager.rum +=balance.quantity;
+					break;
+				case ResourceType.CREW:
+					resourceManager.crew += balance.quantity;
+					break;
+				case ResourceType.FOOD:
+					resourceManager.food += balance.quantity;
+					break;
+				case ResourceType.GUNS:
+					resourceManager.guns += balance.quantity;
+					break;
+				case ResourceType.BOOTY:
+					resourceManager.booty += balance.quantity;
+					break;
+				case ResourceType.PIECES:
+					resourceManager.pieces += balance.quantity;
+					break;
+			}
+		}
+	}
+	
+	void GetBadDialogButton(int n) {
+		foreach (Balance balance in curBadEvent.answers[n].balances)
+		{
+			switch (balance.type)
+			{
+				case ResourceType.RUM:
+					resourceManager.rum +=balance.quantity;
+					break;
+				case ResourceType.CREW:
+					resourceManager.crew += balance.quantity;
+					break;
+				case ResourceType.FOOD:
+					resourceManager.food += balance.quantity;
+					break;
+				case ResourceType.GUNS:
+					resourceManager.guns += balance.quantity;
+					break;
+				case ResourceType.BOOTY:
+					resourceManager.booty += balance.quantity;
+					break;
+				case ResourceType.PIECES:
+					resourceManager.pieces += balance.quantity;
+					break;
+			}
+		}
+	}
+
 
 }
